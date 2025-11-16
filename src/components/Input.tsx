982245bd66e4a469b77bb642209aa6c310d4e7eb
AndreyCubas <felipedreycubascardoso@gmail.com>
2025-11-15 21:13:26 -0300

    feat: Atualizando o estilo do Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,10 +2,10 @@ import React from "react"
 
 type InputProps = {    
     className?: string;
-} & Omit<React.ComponentProps<'input'>, 'className'>; //nao entendi isso mas tem que ter
+} & Omit<React.ComponentProps<'input'>, 'className'>; 
 
 const Input = React.forwardRef<HTMLInputElement, InputProps> (({ className , ...rest}, ref) => {
-    const base = "font-semibold py-2 px-4 rounded duration-200";
+    const base = "peer h-10 w-full bg-[#f8f8f8] px-[15px] py-0 placeholder-transparent focus:outline-none";
     const classes = `${base} ${className || ""}`;
 
     return <input ref={ref} className={classes} {...rest} />;
