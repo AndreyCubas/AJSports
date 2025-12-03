c5d21d2793bcd029ba1655393a68a6500db7117b
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2025-12-03 17:03:08 -0300

    Rota da home

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Contact from "../pages/contact/contato";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import Contato from "../pages/contact/contato";
 import Login from "../pages/Login/Login";
 import Register from "../pages/register/Register";
 import ForgotPassword from "../pages/forgotPassword/forgotPassword";
 import Cart from "../pages/carrinho/carrinho";
+import Catalogo from "../pages/catalogo/catalogo";
+import Home from "../pages/home/home";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/" replace />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contato" element={<Contato />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
         <Route path="/carrinho" element={<Cart/>}/>
+        <Route path="/catalogo" element={<Catalogo />} />
       </Routes>
     </Router>
   );
